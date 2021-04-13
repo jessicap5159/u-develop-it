d1def0dac948a624e59750de81b1c65c73581550
jessicap5159 <jessicaj5159@gmail.com>
2021-04-12 19:59:37 -0700

    build the database calls

--- server.js
+++ server.js
@@ -29,8 +29,36 @@ console.log('Connected to the election database.')
 
 
 // Returns all data in the candidates table
-db.query(`SELECT * FROM candidates`, (err,rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err,rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err,result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 // Handle user requests that aren't supported by the app
